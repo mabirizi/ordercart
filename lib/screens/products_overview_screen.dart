f7c3941843ccb9affc1c75edc15a05897ac3d8fc
mabirizi <mabiriziad@gmail.com>
2019-10-23 11:17:07 +0300

    created the product model

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class PoductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts =[
+    
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

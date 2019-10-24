66ad58fbac3dfd1307ac9abbf9e2abaecd1c09ef
mabirizi <mabiriziad@gmail.com>
2019-10-24 12:47:29 +0300

    connecting the cart provider

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -15,10 +15,14 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {...items};
+  }
+
+  int get itemCount{
+    return _items.length;
   }
 
   void addItem(
@@ -33,7 +37,7 @@ class Cart with ChangeNotifier {
               id: existingCartItem.id,
               title: existingCartItem.title,
               price: existingCartItem.price,
-              quantity: existingCartItem.quantity));
+              quantity: existingCartItem.quantity + 1));
     } else {
       _items.putIfAbsent(
           productId,
@@ -44,5 +48,6 @@ class Cart with ChangeNotifier {
                 quantity: 1,
               ));
     }
+    notifyListeners();
   }
 }
